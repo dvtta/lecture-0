b7d929d01986743f8a89295374594b87b54abc5f
dvtta <wizard19@gmail.com>
2020-05-24 21:32:07 +0200

    testing merge conflict1

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print ("Hello World from local machine")
 print ("Writing something to be pulled from Github to local computer")
-print ("Merge conflict testing")
+print ("Merge conflict testing2")
 
